2769a6f182a81667e095e566a20bd7e090f69ef4
Jason Deutsch <jndeutsch@gmail.com>
2015-08-12 21:18:42 -0400

    ugly commit, progress in admin side, deny client updates on user obj

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,15 @@
 // Client Publications
 
-// Onboarding Publications
+// Onboarding 
 
 Meteor.publish('docs', function(currentUser) {
   return Docs.find({userId: currentUser});
+});
+
+
+// Admin 
+Meteor.publish('clientRecords', function(currentUser) {
+  if (currentUser.profile.isAdmin) {
+     return Users.find();
+  }
 });
