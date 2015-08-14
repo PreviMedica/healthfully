4e3e51a8a9ccaa4c993213bc3b08e7656e572e38
Jason Deutsch <jndeutsch@gmail.com>
2015-08-14 11:42:43 -0400

    basic data clientView

--- client/scripts/admin/clientView.js
+++ client/scripts/admin/clientView.js
@@ -2,14 +2,13 @@ Template.clientView.helpers({
   client: function() {
     var userId = Router.current().params._id;
     return Users.findOne({_id: userId});
-
+  },
+  clientDocs: function() {
+    var userId = Router.current().params._id;
+    return Docs.findOne({userId: userId});
   }
 });
 
 Template.clientView.events({
-  'click h2': function() {
-    var userId = Router.current().params._id;
-    console.log(userId);
-  }
 
 });
